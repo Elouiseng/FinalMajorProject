32b0628301c1139e513603b8bfc8e9bb1244abb9
Ela Glatzeder <122558521+Elouiseng@users.noreply.github.com>
2024-03-17 18:36:02 +0000

    Changed Layout （￣。。￣）

--- Assets/Scripts/PathfinderScript.cs
+++ Assets/Scripts/PathfinderScript.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PathfinderScript : MonoBehaviour
 {
+    List<Transform> wayPoints;
+    int wayPointIndex = 0;
+
+    private void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
