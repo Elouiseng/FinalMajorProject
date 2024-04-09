fb40c1ac1b1ea448900807ba9ffed3fc6032c72e
Ela Glatzeder <122558521+Elouiseng@users.noreply.github.com>
2024-04-09 19:24:59 +0100

    （*＾-＾*）

---
+++ Assets/Scripts/GameplayScripts/CustomerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class CustomerScript : MonoBehaviour
+{
+    private GameObject[] items;
+    [SerializeField] GameObject orderSlot;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = GameObject.FindGameObjectsWithTag("Item");
+
+
+        Invoke("ShowWantedItem", 3.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ShowWantedItem()
+    {
+        if (items.Length > 0)
+        {
+            int randomIndex = Random.Range(0, items.Length);
+            
+            orderSlot.GetComponent<SpriteRenderer>().sprite = items[randomIndex].GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+}
---
+++ Assets/Scripts/GameplayScripts/CustomerSpawnerScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawnerScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] customerPrefabs; 
+    [SerializeField] int amountCustomersInLevel;
+    [SerializeField] Vector3 spawnPosition;
+
+    private float minSpawnTime = 10;
+    private float maxSpawnTime = 15;
+    private int spawnCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        spawnCount = 0;
+
+        StartCoroutine(SpawnCustomers());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnCustomers()
+    {
+        while (spawnCount < amountCustomersInLevel)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnTime, maxSpawnTime));
+
+            if (spawnCount < amountCustomersInLevel)
+            {
+                GameObject customerPrefab = customerPrefabs[Random.Range(0, customerPrefabs.Length)];
+
+                Instantiate(customerPrefab, spawnPosition, Quaternion.identity);
+
+                spawnCount++;
+            }
+        }
+    }
+}
